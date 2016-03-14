febe8c432586cbf3397598af7a3e66ac78b6725e
Kazuya Hara <kazuya@9bar.tokyo>
2016-03-15 00:20:07 +0900

    add Rspec ActiveJob setting

--- partials/_rspec.rb
+++ partials/_rspec.rb
@@ -1,3 +1,4 @@
+# for RSpec
 puts "Installing RSpec ... "
 run "bin/rails generate rspec:install"
 puts "\n"
@@ -30,11 +31,35 @@ puts "Adding binstub ..."
 run "bundle binstubs rspec-core"
 puts "\n"
 
+# for Guard & Spring
 puts "Integrating with Guard & Spring ..."
 run "bundle exec guard init rspec"
 run "bundle exec spring binstub rspec"
 gsub_file 'Guardfile', /guard :rspec, cmd: "bundle exec rspec" do/, "guard :rspec, cmd: 'bundle exec spring rspec' do"
 puts "\n"
+
+# for RSpec ActiveJob
+puts "Setup RSpec ActiveJob ..."
+insert_into_file 'coifng/environments/test.rb',%(
+
+# for RSpec ActiveJob
+config.active_job.queue_adapter = :test
+), after: '# config.action_view.raise_on_missing_translations = true'
+insert_into_file 'spec/rails_helper.rb',%(
+require 'rspec/active_job'
+), after: "require 'shoulda/matchers'"
+insert_into_file 'spec/rails_helper.rb',%(
+
+  config.include(RSpec::ActiveJob)
+
+  # clean out the queue after each spec
+  config.after(:each) do
+    ActiveJob::Base.queue_adapter.enqueued_jobs = []
+    ActiveJob::Base.queue_adapter.performed_jobs = []
+  end
+), after: 'config.include FactoryGirl::Syntax::Methods'
+puts "\n"
+
 
 git :add => '.'
 git :commit => "-aqm 'Configured RSpec.'"
