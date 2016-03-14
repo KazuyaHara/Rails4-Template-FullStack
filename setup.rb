3bab6c5e42d6b5f3b113e953fee7204502ced298
Kazuya Hara <kazuya@9bar.tokyo>
2016-03-14 18:53:49 +0900

    create setup.rb

---
+++ setup.rb
@@ -0,0 +1,12 @@
+# ----------------------------------------------------------------
+# Rails4 FullStack Application Template Setup
+# ----------------------------------------------------------------
+
+# Directories for template partials and static files
+@template_root = File.expand_path(File.join(File.dirname(__FILE__)))
+@partials     = File.join(@template_root, 'partials')
+@static_files = File.join(@template_root, 'files')
+
+puts "\n========================================================="
+puts " Rails4 FullStack Application Template Setup"
+puts "=========================================================\n"
