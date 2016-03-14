1af7e1e962bf3a2e7d7cd638cc6db3ce86e5b570
Kazuya Hara <kazuya@9bar.tokyo>
2016-03-14 18:36:57 +0900

    add message in README.md

--- partials/_cleanup.rb
+++ partials/_cleanup.rb
@@ -1,5 +1,8 @@
 puts "Removing unnecessary files ... "
 remove_file "README.rdoc"
-add_file "README.md"
+file 'README.md', <<-CODE.gsub(/^ {2}/, '')
+  # #{app_name}
+  Hi, there! Good to see you.
+CODE
 
 puts "\n"
