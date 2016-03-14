48de0107000965cd6aa896218da86b2740fd3665
Kazuya Hara <kazuya@9bar.tokyo>
2016-03-14 17:52:23 +0900

    create template.rb

---
+++ template.rb
@@ -0,0 +1,20 @@
+# ----------------------------------------------------------------
+# Rails4 FullStack Application Template
+# ----------------------------------------------------------------
+
+# Directories for template partials and static files
+@template_root = File.expand_path(File.join(File.dirname(__FILE__)))
+@partials     = File.join(@template_root, 'partials')
+@static_files = File.join(@template_root, 'files')
+
+# Copy a static file from the template into the new application
+def copy_static_file(path)
+  puts "Installing #{path}..."
+  remove_file path
+  file path, File.read(File.join(@static_files, path))
+  puts "\n"
+end
+
+puts "\n========================================================="
+puts " Rails4 FullStack Application Template"
+puts "=========================================================\n"
