9c42ffdb4b723ea6dd3ecf50294369dcddf42af7
Kazuya Hara <kazuya@9bar.tokyo>
2016-03-14 19:23:26 +0900

    update format

--- partials/_application.rb
+++ partials/_application.rb
@@ -6,19 +6,19 @@ gsub_file 'config/application.rb', /# config.i18n.default_locale = :de/, "config
 
 puts "Setting default generator... "
 generators = <<-RUBY
-  config.generators do |g|
-    g.stylesheets false
-    g.javascripts false
-    g.helper false
-    # g.test_framework :rspec,
-    #   fixtures: true,
-    #   view_specs: false,
-    #   helper_specs: false,
-    #   routing_specs: false,
-    #   controller_specs: true,
-    #   request_specs: false
-    # g.fixture_replacement :factory_girl, dir: "spec/factories"
-  end
+config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      # g.test_framework :rspec,
+      #   fixtures: true,
+      #   view_specs: false,
+      #   helper_specs: false,
+      #   routing_specs: false,
+      #   controller_specs: true,
+      #   request_specs: false
+      # g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
 RUBY
 application generators
 
