fcb7e3a107d39028549832d166758254f8431708
Kazuya Hara <kazuya@9bar.tokyo>
2016-03-15 01:26:47 +0900

    add kaminari & meta-tags setting

--- partials/_essentials.rb
+++ partials/_essentials.rb
@@ -45,3 +45,59 @@ puts "\n"
 puts "Installing friendly_id ..."
 run "bundle exec rails g friendly_id; bundle exec rake db:migrate"
 puts "\n"
+
+# for kaminari
+puts "Installing kaminari ..."
+run "bundle exec rails g kaminari:config"
+run "bundle exec rails g kaminari:views bootstrap3"
+puts "\n"
+
+# for meta-tags
+puts "Setting meta-tags ..."
+gsub_file 'app/views/layouts/application.html.haml', /%title Appname/, "= render partial: 'layouts/meta_tags'"
+file 'app/views/layouts/_meta_tags.html.haml', <<-CODE.gsub(/^ {2}/, '')
+%meta{charset: 'UTF-8'}
+%meta{name: 'viewport', content: 'width=device-width, initial-scale=1.0'}
+%meta{content: 'NOYDIR', name: 'ROBOTS'}
+%meta{content: 'NOODP', name: 'ROBOTS'}
+= display_meta_tags default_meta_tags
+CODE
+insert_into_file 'app/helpers/application_helper.rb',%(
+  def default_meta_tags
+    {
+      site: Settings.site[:name],
+      reverse: true,
+      description: Settings.site[:page_description],
+      og: {
+        title: Settings.site[:name],
+        type: Settings.site[:meta][:og][:type],
+        url: root_url,
+        image: image_url(Settings.site[:meta][:og][:image]),
+        site_name: :site,
+        description: :description,
+        locale: 'ja_JP'
+      },
+      twitter: {
+        card: Settings.site[:meta][:twitter][:card],
+        site: Settings.site[:meta][:twitter][:site],
+        title: Settings.site[:name],
+        description: Settings.site[:page_description],
+        image: {_: image_url(Settings.site[:meta][:og][:image])}
+        }
+      }
+  end), after: 'module ApplicationHelper'
+remove_file 'config/settings.yml'
+file 'config/settings.yml', <<-CODE.gsub(/^ {2}/, '')
+site:
+  name: "#{app_name}"
+  page_description: ""
+
+  meta:
+    og:
+      type: 'website'
+      image: 'sample.jpg'
+    twitter:
+      card: 'summary_large_image'
+      site: '@XXXX'
+CODE
+puts "\n"
