167dfd0c639f8ea0fce42b3d7cd9204ae62dc1e0
Kazuya Hara <kazuya@9bar.tokyo>
2016-03-14 19:54:43 +0900

    cosmetic change

--- partials/_rspec.rb
+++ partials/_rspec.rb
@@ -1,8 +1,10 @@
 puts "Installing RSpec ... "
 run "bin/rails generate rspec:install"
+puts "\n"
 
 puts "Coloring RSpec output ... "
 run "echo '--color -f d' > .rspec"
+puts "\n"
 
 puts "Updating spec/rails_helper ..."
 insert_into_file 'spec/rails_helper.rb',%(
@@ -11,6 +13,7 @@ require 'shoulda/matchers'
 insert_into_file 'spec/rails_helper.rb',%(
   config.include FactoryGirl::Syntax::Methods
 ), after: 'RSpec.configure do |config|'
+puts "\n"
 
 puts "Updating generator ..."
 gsub_file 'config/application.rb', /# g.test_framework :rspec,/, "g.test_framework :rspec,"
@@ -21,14 +24,17 @@ gsub_file 'config/application.rb', /#   routing_specs: false,/, "  routing_specs
 gsub_file 'config/application.rb', /#   controller_specs: true,/, "  controller_specs: true,"
 gsub_file 'config/application.rb', /#   request_specs: false/, "  request_specs: false"
 gsub_file 'config/application.rb', /# g.fixture_replacement :factory_girl/, "g.fixture_replacement :factory_girl"
+puts "\n"
 
 puts "Adding binstub ..."
 run "bundle binstubs rspec-core"
+puts "\n"
 
 puts "Integrating with Guard & Spring ..."
 run "bundle exec guard init rspec"
 run "bundle exec spring binstub rspec"
 gsub_file 'Guardfile', /guard :rspec, cmd: "bundle exec rspec" do/, "guard :rspec, cmd: 'bundle exec spring rspec' do"
+  puts "\n"
 
 git :add => '.'
 git :commit => "-aqm 'Configured RSpec.'"
