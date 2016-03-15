08eed7957ff80ecc2df86ff6037eee52240a58a7
Kazuya Hara <kazuya@9bar.tokyo>
2016-03-15 14:32:41 +0900

    add compass

--- partials/_stylesheets.rb
+++ partials/_stylesheets.rb
@@ -14,11 +14,24 @@ puts "\n"
   install_from_gemfile
   puts "\n"
 
-  puts "Updating application.scss & application.js"
+  puts "Updating application.scss & application.js ..."
   insert_into_file 'app/assets/stylesheets/application.scss',%(
   @import "bootstrap";), after: "// Add additional styles below this line"
   insert_into_file 'app/assets/stylesheets/application.js',%(
   //= require tether
   //= require bootstrap-sprockets), after: "//= require jquery_ujs"
+  puts "\n"
+end
+
+# for compass
+@user_compass = if yes?('Use compass?([yes or ELSE])')
+  puts "Installing compass ..."
+  gsub_file 'Gemfile', /# gem 'compass-rails'/, "gem 'compass-rails'"
+  install_from_gemfile
+  puts "\n"
+
+  puts "Updating application.scss ..."
+  insert_into_file 'app/assets/stylesheets/application.scss',%(
+  @import "compass";), after: "@import "bootstrap";"
   puts "\n"
 end
