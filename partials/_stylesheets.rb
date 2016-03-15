f934a154101bdba091008eafb072fdf68c89220b
Kazuya Hara <kazuya@9bar.tokyo>
2016-03-15 14:14:36 +0900

    cosmetic change

--- partials/_stylesheets.rb
+++ partials/_stylesheets.rb
@@ -1,4 +1,5 @@
 # convert css to scss
+puts "convert application.css to application.scss"
 remove_file "app/assets/stylesheets/application.css"
 copy_static_file 'app/assets/stylesheets/application.scss'
 puts "\n"
