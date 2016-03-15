6c333830273ad2aea4b445d01d4063c88d09bb89
Kazuya Hara <kazuya@9bar.tokyo>
2016-03-15 16:35:48 +0900

    update stylesheets

--- partials/_stylesheets.rb
+++ partials/_stylesheets.rb
@@ -5,7 +5,8 @@ copy_static_file 'app/assets/stylesheets/application.scss'
 puts "\n"
 
 # for Bootstrap
-@using_bootstrap = if yes?('Use bootstrap4?([yes or ELSE])')
+if yes?('Use bootstrap4?([yes or ELSE])')
+@using_bootstrap = true
 puts "Installing bootstrap4 ..."
 gsub_file 'Gemfile', /# gem 'bootstrap', '~> 4.0.0.alpha3'/, "gem 'bootstrap', '~> 4.0.0.alpha3'"
 gsub_file 'Gemfile', /# rails-assets-source/, "source 'https://rails-assets.org'"
@@ -24,40 +25,42 @@ puts "\n"
 end
 
 # for compass
-@using_compass = if yes?('Use compass?([yes or ELSE])')
+if yes?('Use compass?([yes or ELSE])')
+@using_compass = true
 puts "Installing compass ..."
 gsub_file 'Gemfile', /# gem 'compass-rails'/, "gem 'compass-rails'"
 install_from_gemfile
 puts "\n"
 
 puts "Updating application.scss ..."
-if @using_bootstrap == "yes"
-  insert_into_file 'app/assets/stylesheets/application.scss',%(
-  @import "compass";), after: '@import "bootstrap";'
+if @using_bootstrap
+insert_into_file 'app/assets/stylesheets/application.scss',%(
+@import "compass";), after: '@import "bootstrap";'
 else
-  insert_into_file 'app/assets/stylesheets/application.scss',%(
-  @import "compass";), after: '// Add additional styles below this line'
+insert_into_file 'app/assets/stylesheets/application.scss',%(
+@import "compass";), after: '// Add additional styles below this line'
 end
 puts "\n"
 end
 
 # for Font Awesome
-@using_fontawesome = if yes?('Use font awesome?([yes or ELSE])')
+if yes?('Use font awesome?([yes or ELSE])')
+@using_fontawesome = true
 puts "Installing Font Awesome ..."
 gsub_file 'Gemfile', /# gem 'font-awesome-rails'/, "gem 'font-awesome-rails'"
 install_from_gemfile
 puts "\n"
 
 puts "Updating application.scss ..."
-if @using_compass == "yes"
-  insert_into_file 'app/assets/stylesheets/application.scss',%(
-  @import "font-awesome";), after: '@import "compass";'
-elsif @using_bootstrap == "yes"
-  insert_into_file 'app/assets/stylesheets/application.scss',%(
-  @import "font-awesome";), after: '@import "bootstrap";'
+if @using_compass
+insert_into_file 'app/assets/stylesheets/application.scss',%(
+@import "font-awesome";), after: '@import "compass";'
+elsif @using_bootstrap
+insert_into_file 'app/assets/stylesheets/application.scss',%(
+@import "font-awesome";), after: '@import "bootstrap";'
 else
-  insert_into_file 'app/assets/stylesheets/application.scss',%(
-  @import "font-awesome";), after: '// Add additional styles below this line'
+insert_into_file 'app/assets/stylesheets/application.scss',%(
+@import "font-awesome";), after: '// Add additional styles below this line'
 end
 puts "\n"
 end
